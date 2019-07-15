de9f2f84330d4f11e98e0c36637217315ea8db63
Yarisma <ymendo0101@gmail.com>
2019-07-15 16:22:43 -0500

    Add categories information to product views

---
+++ db/migrate/20190712021442_remove_product_id_from_orders.rb
@@ -0,0 +1,4 @@
+class RemoveProductIdFromOrders < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_233851) do
+ActiveRecord::Schema.define(version: 2019_07_12_020618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carted_products", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.string "status"
+    t.integer "order_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
